c64071e0d5b9ab083719e9358305e49306d2bddb
Ilaiyaraja A <72187756+Ilaiyaraja221197@users.noreply.github.com>
2021-12-23 08:26:54 +0530

    Update omni_test_func.php

--- omni_test_func.php
+++ omni_test_func.php
@@ -1,90 +1,6 @@
 <?php
 ini_set("display_errors",1);
 $a="";
-function omnichannel_create($data){
-	//files move to source to destination
-		
-		//ini_set('max_execution_time', 300); //300 seconds = 5 minutes
-		$src = $_SERVER['DOCUMENT_ROOT']."/ms-sso.zip";		
-		$dst = $_SERVER['DOCUMENT_ROOT']."/".$data['company_name'];
-			// Make sure source folder have sufficient permission to read files
-  
-		$unzip = unzip_folder($src, $dst);
-		
-		$path = $_SERVER['DOCUMENT_ROOT']."/".$data['company_name']."/ms-sso";
-		$string_to_replace="https://omni.mconnectapps.com/ms-sso/";
-        $replace_with="https://omni.mconnectapps.com/".$data['company_name']."/".$a."
-        ms-sso/";
-		
-		multipleFileStrReplace($path,$string_to_replace,$replace_with);
-			return "1";
-		
-		
-    	
-}
-	
-
-
-
-/*Multiple file str change */
-function multipleFileStrReplace($path,$string_to_replace,$replace_with){
-    
-    $fileList = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path), \RecursiveIteratorIterator::SELF_FIRST);
-    
-    foreach ($fileList as $item) {
-        if ($item->isFile()) {
-            $file_contents = file_get_contents($item->getPathName());
-            $file_contents = str_replace($string_to_replace,$replace_with,$file_contents);
-            file_put_contents($item->getPathName(),$file_contents);
-        }
-    }
-}
-
-function unzip_folder($src,$dst){
-	//extract zip file
-	// Create new zip class
-	$zip = new ZipArchive; 
-	// Add zip filename which need
-	// to unzip
-	$zip->open($src);
-	// Extracts to current directory
-	$zip->extractTo($dst);
-
-	$zip->close(); 
-
-}
-
-function zip_folder($src,$dst,$fname){
-	 $rootPath = $_SERVER['DOCUMENT_ROOT']."/";
-
-	// Initialize archive object
-	$zip = new ZipArchive();
-	$zip->open($fname, ZipArchive::CREATE | ZipArchive::OVERWRITE);
-
-	// Create recursive directory iterator
-	/** @var SplFileInfo[] $files */
-	$files = new RecursiveIteratorIterator(
-		new RecursiveDirectoryIterator($rootPath),
-		RecursiveIteratorIterator::LEAVES_ONLY
-	);
-
-	foreach ($files as $name => $file)
-	{
-		// Skip directories (they would be added automatically)
-		if (!$file->isDir())
-		{
-			// Get real and relative path for current file
-			$filePath = $file->getRealPath();
-			$relativePath = substr($filePath, strlen($rootPath) );
-
-			// Add current file to archive
-			$zip->addFile($filePath, $relativePath);
-		}
-	}
-	// Zip archive will be created only after closing object
-	$zip->close();
-
-}
 
 function copy_folder($src, $dst) { 
    
@@ -125,6 +41,4 @@ echo $src = $_SERVER['DOCUMENT_ROOT']."/tests/ms-sso";
 echo $dst = $_SERVER['DOCUMENT_ROOT']."/tests".$a."/".$data['company_name']."/ms-sso";
 copy_folder($src, $dst);
 
-$string_to_replace = "https://omni.mconnectapps.com/ms-sso/";
-$replace_with = "https://omni.mconnectapps.com/".$data['company_name']."/".$a."ms-sso/";
-multipleFileStrReplace($dst,$string_to_replace,$replace_with);
+
